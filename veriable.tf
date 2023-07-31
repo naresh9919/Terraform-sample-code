63f53917b75609252e855a7c9719f2a8af2c171a
VelocityVerse <110754714+naresh9919@users.noreply.github.com>
2023-07-31 21:35:35 +0530

    Create veriable.tf

---
+++ veriable.tf
@@ -0,0 +1,72 @@
+variable "location" {
+    default = "ap-south-1"
+}
+
+variable "os-name" {
+    default = "ami-0f5ee92e2d63afc18"
+}
+
+variable "key-pair" {
+    default = "ubuntukey"
+}
+
+variable "instance-type" {
+    default = "t2.small"
+}
+
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+  
+}
+
+variable "subnet2-cidr" {
+    default = "10.10.2.0/24"
+  
+}
+
+variable "subnet3-cidr" {
+    default = "10.10.3.0/24"
+  
+}
+
+variable "subnet4-cidr" {
+    default = "10.10.4.0/24"
+  
+}
+
+variable "subnet5-cidr" {
+    default = "10.10.5.0/24"
+  
+}
+
+variable "subnet6-cidr" {
+    default = "10.10.6.0/24"
+  
+}
+variable "subent_az1" {
+    default =  "ap-south-1a"  
+}
+
+variable "subent_az2" {
+    default =  "ap-south-1a"  
+}
+
+variable "subent_az3" {
+    default =  "ap-south-1a"  
+}
+
+variable "subent_az4" {
+    default =  "ap-south-1b"  
+}
+
+variable "subent_az5" {
+    default =  "ap-south-1b"  
+}
+
+variable "subent_az6" {
+    default =  "ap-south-1b"  
+}
