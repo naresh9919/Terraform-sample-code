132a3b9d468f2f812325acb22a7c23932d930155
VelocityVerse <110754714+naresh9919@users.noreply.github.com>
2023-08-01 11:17:47 +0530

    Update veriable.tf

--- veriable.tf
+++ veriable.tf
@@ -1,3 +1,9 @@
+variable "access_key" {
+  default = "AKIA6JFVU6E4DYY7K6PT"
+  }
+variable "secret_key" {
+  default = "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"
+  }
 variable "location" {
     default = "ap-south-1"
 }
